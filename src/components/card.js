124e5c766b142e163d51df288bbfaa6a80457680
airhorse83 <airhorse83@yahoo.com>
2018-11-30 09:24:33 -0700

    finished clear button functionality

--- src/components/card.js
+++ src/components/card.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+}
+
+
+
 class Card extends Component {
 
     constructor(){
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebfour: '',
-            adjectiveFive: '',
-            contentVisible: false
-        }
+        this.state = INITIAL_STATE;
        
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -37,8 +42,14 @@ class Card extends Component {
     
     handleFormSubmit(event) {
         event.preventDefault()
-        this.setState({ contentVisible: !this.state.contentVisible })
+
+        if(this.state.contentVisible) {
+            this.setState(INITIAL_STATE)
+        } else {
+            this.setState({ contentVisible: true })
+        }  
     }
+
 
 
     render() {
@@ -46,7 +57,7 @@ class Card extends Component {
         const inputData = [
             {title: 'Color', state: this.state.color, name: 'color'},
             {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
-            {title: 'Adjective', state: this.state.adjective, name: 'adjectiveOne'},
+            {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
             {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
 
             {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
@@ -54,14 +65,14 @@ class Card extends Component {
             {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
             {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
 
-            {title: 'Noun', state: this.state.nounTwo, name: 'NounTwo'},
+            {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
             {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
             {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
             {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
 
             {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
             {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
-            {title: 'Celebrity', state: this.state.celebThree, name: 'celebFour'},
+            {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
             {title: 'Adjective', state: this.state.adejctiveFive, name: 'adjectiveFive'},
         ]
 
